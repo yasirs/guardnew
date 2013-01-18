574fd01762d83cda89720b54bdbe5cfb9f9687bd
Yasir Suhail <yasir.suhail@gmail.com>
2013-01-18 12:12:18 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guardnew::Application.config.secret_token = '4e9104119fd379d2d606d37acb5c014c68b70f001737c39fe5d49ef898dab5f7312e4604dc0b9d994b99c6774445fd53d1e8741ac932c7e58ab4c72b92d7c92f'
